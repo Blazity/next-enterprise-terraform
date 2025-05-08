df85031ea3cbdf5eb56585f054638cf1b962b1b9
Tomasz Czechowski <tomasz@czechowski.pl>
2025-05-08 19:58:34 +0200

    Updated VPC name

--- terraform/module/redis/vpc.tf
+++ terraform/module/redis/vpc.tf
@@ -1,4 +1,4 @@
 resource "aws_elasticache_subnet_group" "redis_cluster_vpc" {
-  name       = "${var.project_name}-redis-${env}"
+  name       = "${var.project_name}-redis-${var.env}"
   subnet_ids = var.vpc_subnets
 }
